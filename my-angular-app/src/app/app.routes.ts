2e0ee691d25b7e3de8f2a23548e8cb40673ce63c
huy <nnqhuy2020@gmail.com>
2025-07-02 13:26:22 +0700

    add gdien create

--- my-angular-app/src/app/app.routes.ts
+++ my-angular-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { Create } from './admin/Product/create/create';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'admin/product/create',
+        component:Create
+    }
+];
--- my-angular-app/src/app/app.ts
+++ my-angular-app/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Create } from './admin/Product/create/create';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Create],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'my-angular-app';
+  protected title = 'huy';
 }
