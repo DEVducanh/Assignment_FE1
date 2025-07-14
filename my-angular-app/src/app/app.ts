3c1c9bf64f0ff6c7fc038ddedd9425048834ecb0
Nguyen Duc Anh <nguyenanh191025@gmail.com>
2025-07-14 12:53:53 +0700

    base-Product

--- my-angular-app/src/app/app.ts
+++ my-angular-app/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderLayoutComponent } from "./components/header-layout/header-layout.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderLayoutComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'my-angular-app';
+  
 }
---
+++ my-angular-app/src/app/components/header-layout/header-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'Header-layout',
+    templateUrl: './header-layout.component.html',
+    styleUrl: './header-layout.component.css'
+})
+
+export class HeaderLayoutComponent {
+    
+}
