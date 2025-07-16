6d18f3a2369eac77a8bc3c132563808f52e62f70
Nguyen Duc Anh <nguyenanh191025@gmail.com>
2025-07-16 13:45:40 +0700

    Done List

--- my-angular-app/src/app/pages/admin/categories-list/categories-list.ts
+++ my-angular-app/src/app/pages/admin/categories-list/categories-list.ts
@@ -1,11 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-categories-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './categories-list.html',
-  styleUrl: './categories-list.css'
+  styleUrl: './categories-list.css',
 })
 export class CategoriesList {
-
+  Categories = [
+    {
+      id: 1,
+      name: 'Điện thoại',
+      image: 'https://example.com/images/phone.jpg',
+      description: 'Các dòng smartphone mới nhất',
+      status: true,
+    },
+    {
+      id: 2,
+      name: 'Laptop',
+      image: 'https://example.com/images/phone.jpg',
+      description: 'Các dòng Laptop mới nhất',
+      status: true,
+    },
+  ];
+   handleDelete = (id: number) => {
+    const isMatch = window.confirm("Bạn có chắc muốn xóa");
+    if(isMatch) alert("success")
+  }
 }
